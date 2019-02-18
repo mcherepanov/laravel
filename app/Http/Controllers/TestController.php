328eb75101910d681190f567c5be8556f20ca222
Maxim Cherepanov <mcherepanov@yandex.ru>
2019-02-18 17:48:23 +0300

    test2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Debugbar as Debug;
+use \DB;
 use Illuminate\Http\Request;
 
 
@@ -15,6 +16,9 @@ class TestController extends Controller
         else echo "А вот теперь правильно, категория = " . $category;
         echo "а это id = " . $id;*/
         Debug::info('category='.$category, 'id='. $id);
+        $users = DB::table('users')->where('name','John')->get();
+        Debug::info($users);
+        //dd($users);
         return view('user', compact('id'))->withCategory($category);
     }
 }
